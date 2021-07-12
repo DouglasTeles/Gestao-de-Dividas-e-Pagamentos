9b9bf93a75e679d21dd165121c222b1f974e8cf3
DouglasTeles <douglastelesoliveira@gmail.com>
2021-07-12 14:54:10 -0300

    back-end update

--- SarcBack/src/index.js
+++ SarcBack/src/index.js
@@ -19,6 +19,6 @@ const routes = require('./routes')
 app.use(routes)
 app.use (cors())
 
-app.listen(2424, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log("Server running!")
 })
