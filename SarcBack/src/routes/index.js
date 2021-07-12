921a3bf8caddcbc0e23d363e5c0bc296f463a3c7
DouglasTeles <douglastelesoliveira@gmail.com>
2021-07-12 11:34:35 -0300

    back-end update

--- SarcBack/src/routes/index.js
+++ SarcBack/src/routes/index.js
@@ -6,29 +6,29 @@ const routes = Router()
 
 //Users
 const userController = require('../controllers/userController')
-routes.post('/sarc/user', userController.createUser)//Cadastra usuario
+routes.post('/sarc/user',authenticate.verifyToken, userController.createUser)//Cadastra usuario
 routes.get('/sarc/user' ,userController.listUser)//Lista usuarios
-routes.delete('/sarc/user/:user_id/delete', userController.deleteUser)//Deleta usuario
+routes.delete('/sarc/user/:user_id/delete',authenticate.verifyToken, userController.deleteUser)//Deleta usuario
 
 
 //Contas
 const contController = require('../controllers/contController')
-routes.post('/sarc/cont', contController.createCont)//Cadastrar conta
+routes.post('/sarc/cont',authenticate.verifyToken, contController.createCont)//Cadastrar conta
 routes.get('/sarc/conts', contController.listCont)//Listar todas as contas
-routes.delete('/sarc/cont/:cont_id/delete', contController.deleteCont)//Deletar Conta especifica
+routes.delete('/sarc/cont/:cont_id/delete',authenticate.verifyToken, contController.deleteCont)//Deletar Conta especifica
 
 
 //Debit
 const debtController = require('../controllers/debtController')
-routes.post('/sarc/debt/:user_id/:cont_id/create', debtController.createDbt )//Cadastrar debt para um morador
+routes.post('/sarc/debt/:user_id/:cont_id/create',authenticate.verifyToken, debtController.createDbt )//Cadastrar debt para um morador
 routes.get('/sarc/debts/:user_id', debtController.listDebtUser)//listar debts de um morador
 routes.delete('/sarc/debts/deleteall', debtController.deleteAllDebt)//Deletar todos os debitos
-routes.put('/sarc/debt/update/:debt_id', debtController.updateDebtUser)//Atualizar debt de um morador
-routes.delete('/sarc/debt/delete/:debt_id', debtController.deleteDebtUser)//Deletar debt de um morador
+routes.put('/sarc/debt/update/:debt_id',authenticate.verifyToken, debtController.updateDebtUser)//Atualizar debt de um morador
+routes.delete('/sarc/debt/delete/:debt_id',authenticate.verifyToken, debtController.deleteDebtUser)//Deletar debt de um morador
 
 //Crédit
 const creditController = require('../controllers/creditController')
-routes.post('/sarc/credit/:user_id/new', creditController.createCredit)//Cadastrar crédito
+routes.post('/sarc/credit/:user_id/new',authenticate.verifyToken, creditController.createCredit)//Cadastrar crédito
 routes.get('/sarc/credit/:user_id/list', creditController.listCredit)//Listar crédito
 
 //Total
